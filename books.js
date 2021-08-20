b2b5e2178e81001811f23321c28212776b5d9a1d
eusebiuschipor <eusebiu@eusebiuschipor.com>
2021-08-21 01:18:53 +0300

    First commit

---
+++ books.js
@@ -0,0 +1,6 @@
+/*
+    You have a list of books. For each book you have the button "Add to favorite". When the button is clicked and if the book is not already added, add it to the "Favorite books" 
+    list if the book title does not contain the word "great". If the book title contain the word great add it to the "Favorite great books" list. Also, each time we add a book to 
+    the list we should update the favorite books number or favorite great books number. After the book is added to one of the favorite lists, add for each book the button 
+    "Remove from favorite", which will delete the book from favorite list and will decrement the book number count. Condition: use class and this. 
+*/
